90a0f7c124a5efa89ee486b902de4ff9579d706f
ansi91 <chii23@naver.com>
2020-07-02 17:53:18 +0900

    a

---
+++ app/src/main/java/com/example/basket/ui/menu/purchaseHistory/ItemData.java
@@ -0,0 +1,12 @@
+package com.example.basket.ui.menu.purchaseHistory;
+
+/**
+ * Created by kj.lee on 2017. 9. 26..
+ */
+
+public class ItemData
+{
+    public String strTitle;
+    public String strDate;
+    public String strContent;
+}
--- app/src/main/java/com/example/basket/ui/menu/purchaseHistory/PurchaseHistoryActivity.java
+++ app/src/main/java/com/example/basket/ui/menu/purchaseHistory/PurchaseHistoryActivity.java
@@ -1,16 +1,41 @@
 package com.example.basket.ui.menu.purchaseHistory;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.basket.R;
 
+import java.util.ArrayList;
+
 public class PurchaseHistoryActivity extends AppCompatActivity {
+    private ListView m_oListView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase_history);
+// 데이터 생성.
+        String[] strDate = {"2017-01-03", "1965-02-23", "2016-04-13", "2010-01-01", "2017-06-20",
+                "2012-07-08", "1980-04-14", "2016-09-26", "2014-10-11", "2010-12-24"};
+        int nDatCnt=0;
+        ArrayList<ItemData> oData = new ArrayList<>();
+        for (int i=0; i<10; ++i)
+        {
+            ItemData oItem = new ItemData();
+            oItem.strTitle = "홈플러스 " + (i+1);
+            oItem.strDate = strDate[nDatCnt++];
+            oData.add(oItem);
+            if (nDatCnt >= strDate.length) nDatCnt = 0;
+        }
+
+        // ListView 생성
+        m_oListView = (ListView)findViewById(R.id.aph_listview);
+        ListAdapter oAdapter = new ListAdapter(oData);
+        m_oListView.setAdapter(oAdapter);
     }
 }
